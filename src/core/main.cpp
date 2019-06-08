2a59981c84f2545ed4948afdb199c096ce127a3b
hman523 <shbarbella@gmail.com>
2019-06-08 15:08:53 -0400

    Worked a little, added some sample code, figuring out how to parse

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 
-int main(){
+int main(int argc, char *argv[]){
 	std::cout << "Starting Dali Compiler" << std::endl;
+	if (argc != 2){
+		std::cout << "Need a file to compile" << std::endl;
+		exit(1);
+	}
+
+	
 
 	return 0;
 }
